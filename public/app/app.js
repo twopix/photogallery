1cf18493d5b7db713b12f25dd9a328f5713e64cb
Melnik Maksim <shmaxer@rambler.ru>
2016-10-27 23:41:32 +0500

    Init project

---
+++ public/app/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('employeeRecords', [])
+    .constant('API_URL', '/api/v1/');
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::group(array('prefix' => 'api/v1'), function() {
+
+    // since we will be using this just for CRUD, we won't need create and edit
+    // Angular will handle both of those forms
+    // this ensures that a user can't access api/create or api/edit when there's nothing there
+    Route::resource('employees', 'Employees',
+        array('only' => array('index', 'store', 'update', 'destroy')));
+
+});
